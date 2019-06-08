86919535b373bd4b040391ef435c03676113a72d
Douglas Donohue <falloutelite@gmail.com>
2019-06-08 16:45:23 +0000

    i think this will fix travis finally

--- unicornattractor/settings.py
+++ unicornattractor/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import env
+if os.environ.get('C9_HOSTNAME'):
+    import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
