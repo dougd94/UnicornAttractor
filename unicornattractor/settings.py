21500dc632535818911ad7b286153523bda404fb
Douglas Donohue <falloutelite@gmail.com>
2019-06-11 18:59:20 +0000

    if cloud 9 debug true

--- unicornattractor/settings.py
+++ unicornattractor/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 if os.environ.get('C9_HOSTNAME'):
     import env
-
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = True
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -24,8 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = ['unicorn-fullstack-doug94.c9users.io']
 
