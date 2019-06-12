d3e79bd2bd5af231e51685a5b96c3af42674ee88
Douglas Donohue <falloutelite@gmail.com>
2019-06-12 15:16:10 +0000

    create test_views for accounts

--- accounts/test_forms.py
+++ accounts/test_forms.py
@@ -5,15 +5,12 @@ class CanCreateAccount(TestCase):
     def test_can_create_account(self):
         form = UserRegistrationForm({'username':"TestUser", "password1": "123456qw", "password2": "123456qw", "email": "123@gmail.com"})
         self.assertTrue(form.is_valid())
-        
     def cant_create_account_with_non_matching_password(self):
         form = UserRegistrationForm({'username':"TestUser", "password1": "123456", "password2": "123456qw", "email": "123@gmail.com"})
         self.assertFalse(form.is_valid())
-        
     def cant_without_a_username(self):
         form = UserRegistrationForm({"password1": "123456qw", "password2": "123456qw", "email": "123@gmail.com"})
         self.assertFalse(form.is_valid())
-    
     def cant_pass_empty_string_as_username(self):
         form = UserRegistrationForm({"username":"","password1": "123456qw", "password2": "123456qw", "email": "123@gmail.com"})
         self.assertFalse(form.is_valid())
---
+++ accounts/test_views.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+class TestViews(TestCase):
+    def test_get_home_page(self):
+        page = self.client.get("/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "index.html")
+    def test_get_log_in_page(self):
+        page = self.client.get("/accounts/login/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "login.html")
+    def test_get_register_page(self):
+        page = self.client.get("/accounts/register/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "registration.html")
