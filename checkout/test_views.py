3081b97280876c621099c4857866d653b36f615f
Douglas Donohue <falloutelite@gmail.com>
2019-06-12 15:48:52 +0000

    test_views for checkout

---
+++ checkout/test_views.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+
+def Checkout_test(TestCase):
+    def test_get_checkout_page(self):
+        page = self.client.get("/checkout/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "checkout.html")
+    def test_get_checkout_page_if_wrong_url(self):
+        page = self.client.get("/checkout/")
+        self.assertEqual(page.status_code, 404)
+        self.assertTemplateUsed(page, "checkout22.html")
