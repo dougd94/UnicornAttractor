c457583285a5782ee02b38fddb56bb1f6bcb2b1b
Douglas Donohue <falloutelite@gmail.com>
2019-06-12 15:45:58 +0000

    update test_views for features

--- features/test_views.py
+++ features/test_views.py
@@ -7,8 +7,18 @@ class TestViews(TestCase):
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, "features.html")
         
-   
+    def get_feature_detail_page(self):
+      feature = Feature({"name":"test", "description":"description"})
+      feature.save()
+      page = self.client.get("/feature/{0}".format(feature.id))
+      self.assertEqual(page.status_code, 200)
+      self.assertTemplateUsed(page, "featuredetail.html")
         
-    def test_get_edit_page_for_feature_that_does_not_exist(self):
+    def test_get_page_for_feature_that_does_not_exist(self):
         page = self.client.get("/feature/10")
         self.assertEqual(page.status_code, 404)
+        
+    def create_feature_page(self):
+        page = self.client.get("/feature/new")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, 'featureform.html')
