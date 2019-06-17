aa9052c4528753c73f69951c6d3b910a907ec306
Douglas Donohue <falloutelite@gmail.com>
2019-06-17 16:59:24 +0000

    add test to bug

---
+++ bugs/test_models.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from .models import Bug
+from django.contrib.auth.models import User
+
+
+class TestBugModel(TestCase):
+    
+        
+    def upvotes_are_1(self):
+        # test that has one upvote on creation
+        user=User(username="test", password="test")
+        user.save()
+        feature = Feature(name="Create a Test", author=user)
+        feature.save()
+        self.assertEqual(feature.name, "Create a Test")
+        self.assertEqual(feature.upvotes, 1)
---
+++ bugs/test_views.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+from .models import Bug
+
+class TestViews(TestCase):
+    def test_get_Bug_page(self):
+        page = self.client.get("/bugs/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "bugs.html")
+        
+    def get_Bug_detail_page(self):
+      bug = Bug({"name":"test", "description":"description"})
+      bug.save()
+      page = self.client.get("/bug/{0}".format(bug.id))
+      self.assertEqual(page.status_code, 200)
+      self.assertTemplateUsed(page, "bugdetail.html")
+        
+    def test_get_page_for_bug_that_does_not_exist(self):
+        page = self.client.get("/bug/10")
+        self.assertEqual(page.status_code, 404)
+        
+    def create_bug_page(self):
+        page = self.client.get("/bug/new")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, 'bugform.html')
