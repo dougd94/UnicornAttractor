323f0087e5c2c930cfb5ce4bfca7cd9dfba10bc2
Douglas Donohue <falloutelite@gmail.com>
2019-06-18 20:08:16 +0000

    fix some issue with unclosed div and debug being true on heorku

--- unicornattractor/settings.py
+++ unicornattractor/settings.py
@@ -17,8 +17,7 @@ if os.environ.get('C9_HOSTNAME'):
     # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = True
 else:
-    # DEBUG = False MAKE THIS FALSE AGAIN THIS IS JUST FOR DEBUGGING
-    DEBUG = True
+    DEBUG = False 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
